57990e1551173e6b7b77efd62077e2ca1527975a
Mohammedi Abdenasser <abdenasser.mohammedi@outlook.com>
2025-09-04 04:48:58 +0100

    added direct supabase call for testing

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -9,18 +9,26 @@ import HomePageHeader from "@/components/home-page/home-page.header";
 import HomePageHero from "@/components/home-page/home-page.hero";
 import HomePageHowItHelps from "@/components/home-page/home-page.how-it-helps";
 import { prisma } from "@/lib/prisma";
+import { createClient } from "@/lib/supabase/server";
 import { findOrCreateUser } from "../actions/user-actions";
 
 export default async function Home() {
   const authUser = await findCurrentUser();
 
-  if (authUser) {
-    const { profile } = prisma.user.findUniqueOrThrow({
-      where: { authId: authUser.id },
-    });
-    console.log(profile);
+  try {
+    if (authUser) {
+      const supabase = await createClient();
+      const profiled = await supabase.from("profile").select("*").eq("id", authUser.id).single();
+      console.log(profiled);
+      const { profile } = prisma.user.findUniqueOrThrow({
+        where: { authId: authUser.id },
+      });
+      console.log(profile);
 
-    //redirect("/sessions/");
+      //redirect("/sessions/");
+    }
+  } catch (error) {
+    console.error(error);
   }
 
   return (
