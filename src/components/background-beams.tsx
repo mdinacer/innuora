daf1b7ead88ff84d38d457459fd287d487edf258
Mohammedi Abdenasser <abdenasser.mohammedi@outlook.com>
2025-09-06 18:33:57 +0100

    updated  home page background

--- src/components/background-beams.tsx
+++ src/components/background-beams.tsx
@@ -61,7 +61,7 @@ export const BackgroundBeams = React.memo(({ className }: { className?: string }
   return (
     <div
       className={cn(
-        "absolute inset-0 flex h-full w-full items-center justify-center [mask-repeat:no-repeat] [mask-size:40px]",
+        "fixed inset-0 flex h-full w-full items-center justify-center [mask-repeat:no-repeat] [mask-size:40px]",
         className
       )}
     >
